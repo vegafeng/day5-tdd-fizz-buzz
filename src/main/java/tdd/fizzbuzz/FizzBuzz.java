866a7278d505dcccddbfbc260eaa0d967635110c
Vega Feng <vega.feng@oocl.com>
2025-09-05 11:44:00 +0800

    feat(day5): add service

--- src/main/java/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/tdd/fizzbuzz/FizzBuzz.java
@@ -2,6 +2,15 @@ package tdd.fizzbuzz;
 
 public class FizzBuzz {
     public String countOff(int number) {
+        if (number % 3 == 0 && number % 5 == 0) {
+            return "FizzBuzz";
+        }
+        if (number % 3 == 0) {
+            return "Fizz";
+        }
+        if (number % 5 == 0) {
+            return "Buzz";
+        }
         return String.valueOf(number);
     }
 }
