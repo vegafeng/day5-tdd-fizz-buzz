ef1b4ccb5eaac2a7637c44fd25596bf529a5e4cc
Vega Feng <vega.feng@oocl.com>
2025-09-05 13:47:13 +0800

    test(day5): add UT

--- src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
@@ -6,7 +6,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class FizzBuzzTest {
     @Test
-    public void should_return_normal_number_when_not_divisible_by_3_or_5() {
+    public void should_return_normal_number_when_not_divisible_by_3_or_5_or_7() {
         //given
         int number = 1;
         String result = getResult(number);
@@ -15,7 +15,7 @@ public class FizzBuzzTest {
     }
 
     @Test
-    public void should_return_fizz_when_divisible_by_3_but_not_5() {
+    public void should_return_fizz_when_divisible_by_3_but_not_5_not_7() {
         //given
         int number = 3;
         String result = getResult(number);
@@ -24,7 +24,7 @@ public class FizzBuzzTest {
     }
 
     @Test
-    public void should_return_buzz_when_divisible_by_5_but_not_3() {
+    public void should_return_buzz_when_divisible_by_5_but_not_3_not_7() {
         //given
         int number = 5;
         String result = getResult(number);
@@ -33,18 +33,51 @@ public class FizzBuzzTest {
     }
 
     @Test
-    public void should_return_fizz_buzz_when_divisible_by_3_and_5() {
+    public void should_return_whizz_when_divisible_by_7_but_not_3_not_5() {
+        //given
+        int number = 7;
+        String result = getResult(number);
+        //then
+        assertEquals("Whizz", result);
+    }
+    @Test
+    public void should_return_fizz_buzz_when_divisible_by_3_and_5_not_7() {
         //given
         int number = 15;
         String result = getResult(number);
         //then
         assertEquals("FizzBuzz", result);
     }
+    @Test
+    public void should_return_fizz_buzz_when_divisible_by_3_and_7_and_5() {
+        //given
+        int number = 15;
+        String result = getResult(number);
+        //then
+        assertEquals("FizzWhizz", result);
+    }
+    @Test
+    public void should_return_fizz_buzz_when_divisible_by_5_and_7_not_3() {
+        //given
+        int number = 15;
+        String result = getResult(number);
+        //then
+        assertEquals("BuzzWhizz", result);
+    }
+
+    @Test
+    public void should_return_fizz_buzz_when_divisible_by_3_and_5_and_7() {
+        //given
+        int number = 15;
+        String result = getResult(number);
+        //then
+        assertEquals("FizzBuzzWhizz", result);
+    }
+
     private static String getResult(int number) {
         FizzBuzz fizzBuzz = new FizzBuzz();
         //when
-        String result = fizzBuzz.countOff(number);
-        return result;
+        return fizzBuzz.countOff(number);
     }
 
 
