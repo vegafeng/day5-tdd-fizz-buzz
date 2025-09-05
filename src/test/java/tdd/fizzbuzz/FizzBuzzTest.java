04d8e155cef45010f9beccb383c397d5568c4e95
Vega Feng <vega.feng@oocl.com>
2025-09-05 11:51:09 +0800

    refactor(day5): extract method

--- src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
@@ -9,20 +9,23 @@ public class FizzBuzzTest {
     public void should_return_normal_number_when_not_divisible_by_3_or_5() {
         //given
         int number = 1;
+        String result = getResult(number);
+        //then
+        assertEquals("1", result);
+    }
+
+    private static String getResult(int number) {
         FizzBuzz fizzBuzz = new FizzBuzz();
         //when
         String result = fizzBuzz.countOff(number);
-        //then
-        assertEquals("1", result);
+        return result;
     }
 
     @Test
     public void should_return_fizz_when_divisible_by_3_but_not_5() {
         //given
         int number = 3;
-        FizzBuzz fizzBuzz = new FizzBuzz();
-        //when
-        String result = fizzBuzz.countOff(number);
+        String result = getResult(number);
         //then
         assertEquals("Fizz", result);
     }
@@ -31,9 +34,7 @@ public class FizzBuzzTest {
     public void should_return_buzz_when_divisible_by_5_but_not_3() {
         //given
         int number = 5;
-        FizzBuzz fizzBuzz = new FizzBuzz();
-        //when
-        String result = fizzBuzz.countOff(number);
+        String result = getResult(number);
         //then
         assertEquals("Buzz", result);
     }
@@ -42,9 +43,7 @@ public class FizzBuzzTest {
     public void should_return_fizz_buzz_when_divisible_by_3_and_5() {
         //given
         int number = 15;
-        FizzBuzz fizzBuzz = new FizzBuzz();
-        //when
-        String result = fizzBuzz.countOff(number);
+        String result = getResult(number);
         //then
         assertEquals("FizzBuzz", result);
     }
