a5e67e5b63f3ca0486891d5a48e35d2081b6249e
Vega Feng <vega.feng@oocl.com>
2025-09-05 11:41:33 +0800

    feat(day5): add UT

--- src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
@@ -1,4 +1,54 @@
 package tdd.fizzbuzz;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class FizzBuzzTest {
+    @Test
+    public void should_return_normal_number_when_not_divisible_by_3_or_5() {
+        //given
+        int number = 1;
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        //when
+        String result = fizzBuzz.countOff(number);
+        //then
+        assertEquals("1", result);
+    }
+
+    @Test
+    public void should_return_fizz_when_divisible_by_3_but_not_5() {
+        //given
+        int number = 3;
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        //when
+        String result = fizzBuzz.countOff(number);
+        //then
+        assertEquals("Fizz", result);
+    }
+
+    @Test
+    public void should_return_buzz_when_divisible_by_5_but_not_3() {
+        //given
+        int number = 5;
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        //when
+        String result = fizzBuzz.countOff(number);
+        //then
+        assertEquals("Buzz", result);
+    }
+
+    @Test
+    public void should_return_fizz_buzz_when_divisible_by_3_and_5() {
+        //given
+        int number = 15;
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        //when
+        String result = fizzBuzz.countOff(number);
+        //then
+        assertEquals("FizzBuzz", result);
+    }
+
+
+
 }
