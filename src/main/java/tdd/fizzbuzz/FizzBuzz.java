e51e5954338783503550e65ffdd7cf7bd652be89
Vega Feng <vega.feng@oocl.com>
2025-09-05 13:52:54 +0800

    feat(day5): add service

--- src/main/java/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/tdd/fizzbuzz/FizzBuzz.java
@@ -5,16 +5,32 @@ public class FizzBuzz {
     public static final String FIZZ_BUZZ = "FizzBuzz";
     public static final String FIZZ = "Fizz";
     public static final String BUZZ = "Buzz";
+    public static final String FIZZ_BUZZ_WHIZZ = "FizzBuzzWhizz";
+    public static final String FIZZ_WHIZZ = "FizzWhizz";
+    public static final String BUZZ_WHIZZ = "BuzzWhizz";
+    public static final String WHIZZ = "Whizz";
 
     public String countOff(int number) {
+        if (number % 3 == 0 && number % 5 == 0 && number % 7 == 0) {
+            return FIZZ_BUZZ_WHIZZ;
+        }
         if (number % 3 == 0 && number % 5 == 0) {
             return FIZZ_BUZZ;
+        }
+        if (number % 3 == 0 && number % 7 == 0) {
+            return FIZZ_WHIZZ;
+        }
+        if (number % 5 == 0 && number % 7 == 0) {
+            return BUZZ_WHIZZ;
         }
         if (number % 3 == 0) {
             return FIZZ;
         }
         if (number % 5 == 0) {
             return BUZZ;
+        }
+        if (number % 7 == 0) {
+            return WHIZZ;
         }
         return String.valueOf(number);
     }
