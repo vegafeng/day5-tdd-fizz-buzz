744c45a906e40cfc672b04ca215a19229571757a
Vega Feng <vega.feng@oocl.com>
2025-09-05 11:47:11 +0800

    refactor(day5): extract final variable

--- src/main/java/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/tdd/fizzbuzz/FizzBuzz.java
@@ -1,15 +1,20 @@
 package tdd.fizzbuzz;
 
 public class FizzBuzz {
+
+    public static final String FIZZ_BUZZ = "FizzBuzz";
+    public static final String FIZZ = "Fizz";
+    public static final String BUZZ = "Buzz";
+
     public String countOff(int number) {
         if (number % 3 == 0 && number % 5 == 0) {
-            return "FizzBuzz";
+            return FIZZ_BUZZ;
         }
         if (number % 3 == 0) {
-            return "Fizz";
+            return FIZZ;
         }
         if (number % 5 == 0) {
-            return "Buzz";
+            return BUZZ;
         }
         return String.valueOf(number);
     }
